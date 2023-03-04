a3d0662f92979c9dae1512cf2ac9efb296419878
james <jkuyt39@gmail.com>
2023-03-04 22:04:45 +0000

    meow

---
+++ new/firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+﻿  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyASQEPMe2tBt8h4ohflMThKZ74Qj9LolM0",
+    authDomain: "this-te.firebaseapp.com",
+    projectId: "this-te",
+    storageBucket: "this-te.appspot.com",
+    messagingSenderId: "815151260683",
+    appId: "1:815151260683:web:432e743fde469bc53d530e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);

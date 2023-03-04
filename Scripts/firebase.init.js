6f74480e82b05c9dcc9f9f5ee13c41480a3e74a4
catmeow232 <jkuyt39@gmail.com>
2023-03-04 15:59:12 -0500

    meow

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,13 @@
+﻿
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyASQEPMe2tBt8h4ohflMThKZ74Qj9LolM0",
+    authDomain: "this-te.firebaseapp.com",
+    projectId: "this-te",
+    storageBucket: "this-te.appspot.com",
+    messagingSenderId: "815151260683",
+    appId: "1:815151260683:web:432e743fde469bc53d530e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);

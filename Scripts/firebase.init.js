ff860016e1754514225eac20c199871b8c0f1ab6
james <jkuyt39@gmail.com>
2023-03-05 19:34:54 +0000

    meow

--- Scripts/firebase.init.js
+++ Scripts/firebase.init.js
@@ -1,7 +1,4 @@
-﻿// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+﻿
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,6 +9,3 @@ const firebaseConfig = {
   messagingSenderId: "815151260683",
   appId: "1:815151260683:web:432e743fde469bc53d530e"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);

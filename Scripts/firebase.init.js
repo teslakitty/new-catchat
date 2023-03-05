bda6982e3a8d8769c18e76abf7f5f66df077455c
james <jkuyt39@gmail.com>
2023-03-05 13:02:45 +0000

    meow

--- Scripts/firebase.init.js
+++ Scripts/firebase.init.js
@@ -1,18 +1,23 @@
-﻿
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAnWB8w5djDai7xUq8QmXvm43T8W4trW3w",
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyASQEPMe2tBt8h4ohflMThKZ74Qj9LolM0",
-    authDomain: "this-te.firebaseapp.com",
-    projectId: "this-te",
-    storageBucket: "this-te.appspot.com",
-    messagingSenderId: "815151260683",
-    appId: "1:815151260683:web:432e743fde469bc53d530e"
-  };
+  authDomain: "chat-app64-eae61.firebaseapp.com",
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  databaseURL: "https://chat-app64-eae61-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+  projectId: "chat-app64-eae61",
+
+  storageBucket: "chat-app64-eae61.appspot.com",
+
+  messagingSenderId: "426477258139",
+
+  appId: "1:426477258139:web:62c78f35810adc0b2f8d3e",
+
+  measurementId: "G-YC9BFXBLDK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+

cf8aa4bedd9b516e94da89bf18486196a0ec73fd
james <jkuyt39@gmail.com>
2023-03-05 18:56:38 +0000

    meow

--- Scripts/chat.js
+++ Scripts/chat.js
@@ -2,23 +2,7 @@
 var chatKey = 'maq';
 var friend_id = 'meow';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyASQEPMe2tBt8h4ohflMThKZ74Qj9LolM0",
-  authDomain: "this-te.firebaseapp.com",
-  projectId: "this-te",
-  storageBucket: "this-te.appspot.com",
-  messagingSenderId: "815151260683",
-  appId: "1:815151260683:web:432e743fde469bc53d530e"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 
 
